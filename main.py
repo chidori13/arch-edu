58d86f7961634b702cf80c174de4628d7f90f28c
chidori13 <kbochkanova@yandex.ru>
2024-05-29 01:17:17 +0300

    draft

---
+++ main.py
@@ -0,0 +1,13 @@
+from abc import ABC
+import numpy as np
+
+class Movable(ABC):
+    def get_location() -> np.array:
+
+    def set_location():
+class Move:
+
+    Movable _movable
+    def __init__(self, Movable movable):
+        _movable.set_location()
+        _movable.get_location()
