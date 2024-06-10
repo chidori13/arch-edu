20fcac0dfc236be1b0ba5324814102eb4852d263
chidori13 <kbochkanova@yandex.ru>
2024-06-11 01:56:52 +0300

    all move test, rotate class

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import unittest
 from abc import ABC
 from unittest.mock import Mock
+
 
 
 class Vector:
@@ -31,10 +33,66 @@ class Move:
         self.movable.set_location(Vector.plus(self.movable.get_location(), self.movable.get_velocity()))
 
 
-def main():
-    movable_mock = Mock(spec=Movable)
-    movable_mock.get_location.return_value = Vector(12, 5)
-    movable_mock.get_velocity.return_value = Vector(-7, 3)
-    move_command = Move(movable_mock)
-    move_command.execute()
-    movable_mock.set_location.assert_called_with(Vector(5,8))
+class TestMove():
+    def test_usual_move(self):
+        movable_mock = Mock(spec=Movable)
+        movable_mock.get_location.return_value = Vector(12, 5)
+        movable_mock.get_velocity.return_value = Vector(-7, 3)
+        move_command = Move(movable_mock)
+        move_command.execute()
+        assert(movable_mock.get_location, Vector(5,8))
+
+    def test_impossible_get_location(self):
+        movable_mock = Mock(spec=Movable)
+        attrs = {'get_velocity.return_value': Vector(1,1), 'get_location.side_effect': ValueError}
+        movable_mock.configure_mock(**attrs)
+        move_command = Move(movable_mock)
+        try:
+            move_command.execute()
+        except ValueError:
+            print(f'Невозможно сдвинуть объект без первоначального положения')
+
+    def test_impossible_get_velocity(self):
+        movable_mock = Mock(spec=Movable)
+        attrs = {'get_location.return_value': Vector(1,1), 'get_velocity.side_effect': ValueError}
+        movable_mock.configure_mock(**attrs)
+        move_command = Move(movable_mock)
+        try:
+            move_command.execute()
+        except ValueError:
+            print(f'Невозможно сдвинуть объект, не зная его скорости')
+
+    def test_impossible_set_location(self):
+        movable_mock = Mock(spec=Movable)
+        attrs = {'get_location.return_value': Vector(1, 1), 'get_velocity.return_value': Vector(1, 1),'set_location.side_effect': Exception}
+        movable_mock.configure_mock(**attrs)
+        move_command = Move(movable_mock)
+        try:
+            move_command.execute()
+        except Exception:
+            print(f'Невозможно изменить положение объекта в пространстве')
+
+
+class Angle:
+    def __init__(self, direction: int, n: int):
+        self.n = n
+        self.direction = direction
+
+    def __operate(f, a, b):
+        return Angle(*f(a, b))
+
+
+class Rotable(ABC):
+    def get_angle()->Angle:
+    pass
+    def set_angle(new_value: Angle):
+    pass
+    def get_angular_velocity()->Angle:
+    pass
+
+class Rotate:
+    def __init__(self, rotable: Rotable):
+        self.rotable = rotable
+
+    def execute(self):
+        self.rotable.set_angle(Angle.plus(self._rotable.get_angle(), self._rotable.get_angular_velocity()))
